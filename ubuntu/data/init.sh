a0b7b2f372e3111d63050edc0c9260ce1f31afdb
z184924 <z184924@126.com>
2025-01-07 18:50:47 +0800

    update init script

--- ubuntu/data/init.sh
+++ ubuntu/data/init.sh
@@ -3,6 +3,7 @@ apt update
 apt-get update
 apt install -y sudo
 apt install -y openssh-server
+echo "update and install finish"
 mkdir /run/sshd
 /usr/sbin/sshd &
 mkdir -p /data/home/$username
@@ -10,5 +11,5 @@ useradd $username
 usermod -d /data/home/$username $username
 chown -R $username:$username /data/home/$username
 echo "${username}   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-echo "new user ${username} password"
+echo "setting new user ${username} password"
 passwd $username
